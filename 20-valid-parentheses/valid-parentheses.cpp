05a8d28d82ce2d958bfd2e98aaa2681f762ffb86
Mehmet Efe Bıyık <69343706+Mehmet-b1yk@users.noreply.github.com>
2025-11-10 16:16:15 +0300

    Time: 3 ms (9.66%) | Memory: 8.8 MB (87.31%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.length() % 2 != 0) return false;
+vector<char> opens = {};
+for (int i = 0; i < s.length(); i++) 
+{
+    if (s[i] == '(' || s[i] == '[' || s[i] == '{') opens.push_back(s[i]);
+    else 
+    {
+        if (opens.size() == 0) return false;
+        if (s[i] == ')')
+        {
+            if (opens.back() == '(') opens.pop_back();
+            else return false;
+        }
+        if (s[i] == ']')
+        {
+            if (opens.back() == '[') opens.pop_back();
+            else return false;
+        }
+        if (s[i] == '}')
+        {
+            if (opens.back() == '{') opens.pop_back();
+            else return false;
+        }
+    }
+}
+return opens.size() == 0;
+    }
+};
